4984d692b113d255eb2d6c99569323049dbfd516
nikki-red <97693476+nikki-red@users.noreply.github.com>
2022-12-30 23:43:01 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,38 +1,65 @@
+// Variables used to note the cell numbers if invalid (does not follow sudoku rules)
+var x1 = -1,
+  x2 = -1,
+  y1 = -1,
+  y2 = -1;
+
 function readInput() {
   // Create a 2d array initialised with 0s
   var grid = Array(9).fill().map(() => Array(9).fill(0));
   for (var i = 0; i < 81; i++) {
     var val = document.querySelectorAll('input')[i].value;
-    if (val) {
+    if (val) { // If cell if filled by user
       var a = Math.floor(i / 9),
         b = i % 9;
       // Assigning values from input table to grid
       grid[a][b] = val;
-    } else {
-      document.querySelectorAll('input')[i].classList.add("empty");
+    } else { // If cell is empty
+      // Adding 'empty' class name to empty cells
+      document.querySelectorAll('input')[i].classList.add('empty');
     }
   }
+  // Removing any 'invalidCell' class name from any such cells
+  var boxes = document.querySelectorAll('.invalidCell');
+  boxes.forEach(box => {
+    box.classList.remove('invalidCell');
+  });
   // Check if user input follows sudoku rules
-  if (!checkInput(grid)) {
-    alert("Invalid input");
-    window.location.reload();
+  if (!checkInput(grid, x1, y1, x2, y2)) {
+    //console.log(x1 + " " + y1 + " " + x2 + " " + y2);
+    // Adding 'invalidCell' class name if that cell does not follow sudoku rules
+    document.querySelectorAll('input')[parseInt(x1 * 9) + parseInt(y1)].classList.add('invalidCell');
+    document.querySelectorAll('input')[parseInt(x2 * 9) + parseInt(y2)].classList.add('invalidCell');
+    // Removing any 'empty' class name from any such cells
+    var boxes = document.querySelectorAll('.empty');
+    boxes.forEach(box => {
+      box.classList.remove('empty');
+    });
+    // alert("Invalid input");
+
   } else {
+    // Solve sudoku
     solve(grid);
     var index = 0;
     for (var i = 0; i < 9; i++) {
       for (var j = 0; j < 9; j++) {
+        // Assigning the values to respective input cell
         document.querySelectorAll('input')[index++].value = grid[i][j];
       }
     }
   }
 }
 
-function checkInput(grid) {
+function checkInput(grid, x1, y1, x2, y2) {
   for (var i = 0; i < 9; i++) {
     for (var j = 0; j < 9; j++) {
       if (grid[i][j] != 0) {
-        console.log("cell[" + i + "][" + j + "]: " + grid[i][j]);
-        if (!check(grid, grid[i][j], i, j, true)) return false;
+        //console.log("cell[" + i + "][" + j + "]: " + grid[i][j]);
+        if (!check(grid, grid[i][j], i, j, true, x2, y2)) {
+          this.x1 = i, this.y1 = j;
+          //console.log("horizontal caught @ row " + this.x1 + " j " + this.y1);
+          return false;
+        }
       }
     }
   }
@@ -67,8 +94,7 @@ function emptyCell(grid, row, col) {
   for (var i = x; i < 9; i++) {
     for (var j = y; j < 9; j++) {
       if (grid[i][j] == 0) {
-        this.row = i;
-        this.col = j;
+        this.row = i, this.col = j;
         return true;
       }
     }
@@ -76,12 +102,13 @@ function emptyCell(grid, row, col) {
   return false;
 }
 
-function check(grid, i, row, col, flag) {
+function check(grid, i, row, col, flag, x2, y2) {
   //check horizontal
   for (var j = 0; j < 9; j++) {
     if (flag && j == col) continue;
     if (grid[row][j] == i) {
-      //console.log("horizontal caught @ row " + row + " j " + j);
+      this.x2 = row, this.y2 = j;
+      //console.log("horizontal caught @ row " + this.x2 + " j " + this.y2);
       return false;
     }
   }
@@ -89,6 +116,7 @@ function check(grid, i, row, col, flag) {
   for (var j = 0; j < 9; j++) {
     if (flag && j == row) continue;
     if (grid[j][col] == i) {
+      this.x2 = j, this.y2 = col;
       //console.log("vertical caught @ j " + j + " col " + col);
       return false;
     }
@@ -100,6 +128,7 @@ function check(grid, i, row, col, flag) {
     for (var k = y; k < y + 3; k++) {
       if (flag && j == row && k == col) continue;
       if (grid[j][k] == i) {
+        this.x2 = j, this.y2 = k;
         //console.log("box caught @ j " + j + " k " + k);
         return false;
       }
